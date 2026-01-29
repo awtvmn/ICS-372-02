70b23a3f0c10438ce383959a6a31d6b20656f658
awtvmn <anishagurung257@gmail.com>
2026-01-28 20:45:43 -0600

    copied and pasted json-simple into dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
