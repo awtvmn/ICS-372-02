76f2ac1424c2a18c27c694470ad119661030cabe
ahass_q84i0tq <ahassan5557@gmail.com>
2026-02-03 13:49:31 -0600

    was able to add an array of items in Main class and a toString method in itemclass

--- src/main/java/edu/metrostate/Item.java
+++ src/main/java/edu/metrostate/Item.java
@@ -3,10 +3,10 @@ package edu.metrostate;
 public class Item {
     private String name;
     private double price;
-    private int quantity;
+    private long quantity;
 
     //constructor
-    public Item(String name, double price, int quantity) {
+    public Item(String name, double price, long quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
@@ -15,11 +15,15 @@ public class Item {
     //getters
     public String getName() {return name;}
     public double getPrice() {return price;}
-    public int getQuantity() {return quantity;}
+    public long getQuantity() {return quantity;}
 
     //setters
     public void setName(String name) {this.name = name;}
     public void setPrice(float price) {this.price = price;}
-    public void setQuantity(int quantity) {this.quantity = quantity;}
+    public void setQuantity(long quantity) {this.quantity = quantity;}
+@Override
+    public String toString(){
+        return name + ": " + "quantity " + quantity  + " price is " + price ;
+    }
 
 }
--- src/main/java/edu/metrostate/Main.java
+++ src/main/java/edu/metrostate/Main.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -26,20 +25,31 @@ public class Main {
 
             long order_date = (long)order.get("order_date");
             System.out.println("order date is  " + order_date);
-
-            // the following parses from json requested items, quantity,and item price of each order
+            int ID=0;
+            ArrayList<Item> itemList = new ArrayList<>();
+            // the following parses from JSON requested items, quantity,and item price of each order
             JSONArray items = (JSONArray) order.get("items");
             for (Object obj : items){
-                JSONObject item = (JSONObject) obj;
-                String name = (String) item.get("name");
-                long quantity = (long) item.get("quantity");
-                double price = (double) item.get("price");
+                JSONObject Jitem = (JSONObject) obj;
 
+
+                String name = (String) Jitem.get("name");
+                long quantity = (long) Jitem.get("quantity");
+                double price = (double) Jitem.get("price");
+                Item item = new Item(name, price,quantity);
+                itemList.add(item);
                 System.out.println("item  " + name);
                 System.out.println("quantity  " + quantity);
                 System.out.println("price  " + price);
+                System.out.println("ddddddddddddddddddddddddd");
 
             }
+
+            for (Item jitem : itemList){
+                System.out.println(jitem);
+            }
+            ID++;
+            System.out.println("ID is " + ID);
 
 
         } catch (IOException | ParseException e) {
