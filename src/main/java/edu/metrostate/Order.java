8a817bbd2d8cf0a36f529d02689ee17a78386e1f
Anisha <anishagurung257@gmail.com>
2026-02-03 12:14:24 -0600

    Implemented startFulfilling() and completeOrder()

--- src/main/java/edu/metrostate/Order.java
+++ src/main/java/edu/metrostate/Order.java
@@ -5,10 +5,12 @@ import org.json.simple.JSONObject;
 public abstract class Order {
     protected int orderID;
     protected long orderDate;
+    protected OrderStatus status;
 
     public Order(int orderID, long orderDate){
         this.orderID = orderID;
-        this.orderDate = orderDate;;
+        this.orderDate = orderDate;
+        this.status = OrderStatus.INCOMING;
     }
 
     public int getOrderID(){
@@ -18,4 +20,23 @@ public abstract class Order {
     public long getOrderDate(){
         return orderDate;
     }
+    
+    public OrderStatus getOrderStatus() {
+        return status;
+    }
+
+    public boolean startFulfilling() {
+        if (status == OrderStatus.INCOMING){
+            status = OrderStatus.IN_PROGRESS;
+            return true;
+        }
+        return false;
+    }
+     public boolean completeOrder() {
+        if (status == OrderStatus.IN_PROGRESS) {
+            status = OrderStatus.COMPLETED;
+            return true;
+        }
+        return false;
+     }
 }
