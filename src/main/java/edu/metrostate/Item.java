7f3aaeaa81adca9da4f541b24b2aca8f85f6f82d
nat873 <ij8045oh@go.minnstate.edu>
2026-02-03 10:30:01 -0600

    Added item class

--- src/main/java/edu/metrostate/Item.java
+++ src/main/java/edu/metrostate/Item.java
@@ -3,18 +3,22 @@ package edu.metrostate;
 public class Item {
     private String name;
     private float price;
+    private int quantity;
 
     //constructor
-    public Item(String name, float price) {
+    public Item(String name, float price, int quantity) {
         this.name = name;
         this.price = price;
+        this.quantity = quantity;
     }
 
     //getters
     public String getName() {return name;}
     public float getPrice() {return price;}
+    public int getQuantity() {return quantity;}
 
     //setters
     public void setName(String name) {this.name = name;}
     public void setPrice(float price) {this.price = price;}
+    public void setQuantity(int quantity) {this.quantity = quantity;}
 }
