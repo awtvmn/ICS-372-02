a2fd2c49f7171e3dc83eb5fa6026879c98013492
nat873 <ij8045oh@go.minnstate.edu>
2026-02-03 14:10:26 -0600

    Added another order test file and asking for user to input filename

--- src/main/java/edu/metrostate/Main.java
+++ src/main/java/edu/metrostate/Main.java
@@ -3,11 +3,11 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import java.util.ArrayList;
 
 import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -15,9 +15,20 @@ public class Main {
     public static void main(String[] args) {
         //FileReader fr = new FileReader(order.json);
         JSONParser parser = new JSONParser();
+        Scanner s = new Scanner(System.in);
 
         try {
-            JSONObject js = (JSONObject)parser.parse(new FileReader("txt/order.json"));
+            //asking for file name
+            System.out.println("Enter order file (txt/(filename).json): ");
+            String filename = s.nextLine();
+            File file = new File(filename);
+
+            //checkin
+            if(!file.exists()){
+                System.out.println("File does not exist");
+            }
+
+            JSONObject js = (JSONObject)parser.parse(new FileReader(filename));
             System.out.println(js);
             //The software shall read and store the order type
             JSONObject order = (JSONObject) js.get("order");
@@ -27,7 +38,7 @@ public class Main {
             long order_date = (long)order.get("order_date");
             System.out.println("order date is  " + order_date);
 
-            // the following parses from json requested items, quantity,and item price of each order
+            // the following parses from JSON requested items, quantity,and item price of each order
             JSONArray items = (JSONArray) order.get("items");
             for (Object obj : items){
                 JSONObject item = (JSONObject) obj;
@@ -44,12 +55,6 @@ public class Main {
 
         } catch (IOException | ParseException e) {
             throw new RuntimeException(e);
-        }
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
     }
 }
