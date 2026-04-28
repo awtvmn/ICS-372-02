39ec9c05dcc21d6d9295cdf997f0867bca354fd0
KanagX <kanagxiong@gmail.com>
2026-04-28 16:07:57 -0500

    undo unit test

---
+++ src/test/java/UndoManagerTest.java
@@ -0,0 +1,53 @@
+import edu.metrostate.*;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class UndoManagerTest {
+
+    @Test
+    public void undoManagerTest() {
+        OrderManager orderManager = new OrderManager();
+        UndoManager undoManager = new UndoManager();
+
+        ArrayList<Item> items = new ArrayList<>();
+        Order order = new PickupOrder(1, System.currentTimeMillis(), items);
+
+        orderManager.getAllOrders().put(1, order);
+        undoManager.saveState(order);
+        orderManager.startOrder(1);
+        boolean result = undoManager.undo(orderManager);
+
+        assertTrue(result);
+        assertEquals(OrderStatus.INCOMING, order.getOrderStatus());
+
+    }
+
+    @Test
+    public void undoManagerTest2() {
+        OrderManager orderManager = new OrderManager();
+        UndoManager undoManager = new UndoManager();
+
+        ArrayList<Item> items = new ArrayList<>();
+        Order order = new PickupOrder(2, System.currentTimeMillis(), items);
+
+        orderManager.getAllOrders().put(2, order);
+        undoManager.saveState(order);
+        orderManager.cancelOrder(2);
+        boolean result = undoManager.undo(orderManager);
+
+        assertTrue(result);
+        assertEquals(OrderStatus.INCOMING, order.getOrderStatus());
+
+    }
+
+    @Test
+    public void undoManagerTest3() {
+        OrderManager orderManager = new OrderManager();
+        UndoManager undoManager = new UndoManager();
+
+        boolean result = undoManager.undo(orderManager);
+        assertFalse(result);
+    }
+}
